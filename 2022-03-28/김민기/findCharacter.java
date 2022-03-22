a2f0f68955fa057926cfcb0f0518a22c1f0d89f4
Minki Kim <minki0415@naver.com>
2022-03-22 23:28:44 +0900

    Update findCharacter.java

--- 2022-03-28/김민기/findCharacter.java
+++ 2022-03-28/김민기/findCharacter.java
@@ -8,15 +8,18 @@ public class findCharacter {
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str1 = br.readLine().toLowerCase();
+        String str = br.readLine().toLowerCase();
 	char c = br.readLine().toLowerCase().charAt(0);
 
 	int count = 0;
 
-	for(int i = 0; i < str1.length(); i++){
-		if(str1.charAt(i) == c) count++;
-	}
+	// for(int i = 0; i < str1.length(); i++){
+	// 	if(str1.charAt(i) == c) count++;
+	// }
 
+	for(char x : str.toCharArray()){
+		if(x == c) count++;
+	}
 	System.out.println(count);
     }	
 }
