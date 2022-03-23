9859e6a859f2bef4ce8d3188d6198c597ebcdac0
Minki Kim <minki0415@naver.com>
2022-03-23 22:12:36 +0900

    Update and rename findCharacter.java to FindCharacter.java

--- 2022-03-28/김민기/FindCharacter.java
+++ 2022-03-28/김민기/FindCharacter.java
@@ -3,7 +3,7 @@ package week2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class findCharacter {
+public class FindCharacter {
 
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
