2f924b9a3f4f876c0f40242b94378bafc7faa2fb
mandariny <mandariny716@gmail.com>
2022-04-04 23:24:20 +0900

    Greedy

--- 2022-04-04/윤소희/Delivery.java
+++ 2022-04-04/윤소희/Delivery.java
@@ -1,3 +1,5 @@
+//문제 : https://programmers.co.kr/learn/courses/30/lessons/12978
+
 public class Delivery {
 
     public static void main(String[] args) {
--- 2022-04-04/윤소희/FindCities.java
+++ 2022-04-04/윤소희/FindCities.java
@@ -1,3 +1,5 @@
+//문제 : https://www.acmicpc.net/problem/18352
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
