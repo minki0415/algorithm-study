836bd40ab0730401302ba8eecbfa9af37ab82c1f
mandariny <mandariny716@gmail.com>
2022-04-25 23:49:33 +0900

    sql

---
+++ 2022-05-02/윤소희/BinaryTreeNodes.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
+select N, (case
+               when P is null then 'Root'
+               when N in (select P from BST) then 'Inner'
+               else 'Leaf'
+          end)
+from BST
+order by N;
